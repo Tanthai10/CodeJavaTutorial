ca0f642bc5b3261d080880b74d537d6f592e2e9b
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-07-25 14:31:17 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x,y,z;
+        x = 69;
+        y = 99;
+        z = x + y;
+        System.out.print("x = " +x+ " ");
+        System.out.println("y = " +y);
+        System.out.print("Total = " +z);
+    }
+}
