b484eba29942e5803695f65f5cb237ff892c9644
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-01 10:09:47 +0700

    Update Result1To100.java

--- Result1To100.java
+++ Result1To100.java
@@ -1,10 +1,10 @@
 public class Result1To100 {
     public static void main(String[] args) {
-        int iWhile = 1, sum = 0;
-        while (iWhile <= 100){
-            System.out.println("Result in whileloop : "+iWhile);
-            iWhile += 1;
-            sum += iWhile;
+        int sum = 0,i = 1;
+        while (i <= 100){
+            System.out.println("Result in whileloop : "+i);
+            sum += i;
+            i ++;
         }
         System.out.println("total result : " +sum);
     }
