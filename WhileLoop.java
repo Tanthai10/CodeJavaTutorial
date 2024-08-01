bb637b8f10fe1d89ed478e81b4bf9a51737448c1
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-01 10:03:58 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int iWhile = 1;
+        while (iWhile <= 100){
+            System.out.println("Result in whileloop : "+iWhile);
+            iWhile += 1;
+        }
+    }
+}
