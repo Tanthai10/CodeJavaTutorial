4ab1e530f0217f335cc5bb4213bd7d3578e1c18b
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-01 14:20:31 +0700

    Create exNo3.java

---
+++ exNo3.java
@@ -0,0 +1,9 @@
+public class exNo3 {
+    public static void main(String[] args) {
+        int mult = 8;
+        for(int i = 1; i <= 12; i++){
+            int result = mult * i;
+            System.out.println("8 * "+i+ " = " +result);
+        }
+    }
+}
