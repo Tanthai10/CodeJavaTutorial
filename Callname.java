5e0073a7bf6d10d00855e42dc2bb80703769fffc
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-02 10:08:42 +0700

    Create Callname.java

---
+++ Callname.java
@@ -0,0 +1,10 @@
+public class Callname {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            name();
+        }  
+    }
+    public static void name(){
+            System.out.println("Tanthai Suwannawong");
+    }
+}
