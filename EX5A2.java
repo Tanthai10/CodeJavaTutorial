cc187eaad9a83af646a42a9f54ac95db01b549e3
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-02 15:04:57 +0700

    Create EX5A2.java

---
+++ EX5A2.java
@@ -0,0 +1,8 @@
+public class EX5A2 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            int sum = x * x * x;
+            System.out.println("The sum of " + x + " is: " + sum);
+        }
+    }
+}
