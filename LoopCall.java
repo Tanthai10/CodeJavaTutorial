9d1ad005732c5b33d7242445c99ef709ae1e7ce3
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-02 10:08:47 +0700

    Create LoopCall.java

---
+++ LoopCall.java
@@ -0,0 +1,11 @@
+public class LoopCall {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main method.");
+        for(int i = 1; i <= 5; i++){
+            displayMessage();
+        }
+    }
+    public static void displayMessage(){
+        System.out.println("Hello from displayMessage.");
+    }
+}
