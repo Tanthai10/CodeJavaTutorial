8fc501d08afef78f6e5945bc1736026004dc3a69
Tanthai10 <Tanthaisuwannawong10@gmail.com>
2024-08-07 14:41:14 +0700

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    private double length;
+    private double width;
+    public void setLength(double len){
+        length = len;
+    }
+    public void setwidth(double w){
+        width = w;
+    }
+    public double getLength(){
+        return length;
+    }
+    public double getwidth(){
+        return width;
+    }
+    public double getArea(){
+        return length * width;
+    }
+}
